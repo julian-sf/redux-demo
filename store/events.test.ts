befba9c063bb20646ebeac0c7e50694542f91e1b
Julian Coy <julian@coy.cloud>
2020-03-17 00:10:34 -0400

    Remove broken test and added comment

--- store/events.test.ts
+++ store/events.test.ts
@@ -1,35 +1,25 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
-import { eventSlice, getEvents, resetEvents } from './events'
+import { eventSlice, resetEvents } from './events'
 
 import { RootState } from './index'
 
 const mockStore = configureMockStore<Partial<RootState>>([thunk])
 
-describe('people store', () => {
+describe('events store', () => {
   const store = mockStore({})
 
   beforeEach(() => {
     store.clearActions()
   })
 
-  describe('resetPeople', () => {
+  // this isn't a very useful test, but it just kinda goes to show how difficult testing redux stores can be with just
+  // dispatches and actions
+  describe('resetEvents', () => {
     it('sends the right action', () => {
       return store.dispatch<any>(resetEvents()).then(() => {
         expect(store.getActions()).toEqual([eventSlice.actions.resetEvents()])
-      })
-    })
-  })
-
-  describe('getPeople', () => {
-    it('sends the right action', () => {
-      return store.dispatch<any>(getEvents()).then(() => {
-        expect(store.getActions()).toEqual([
-          {
-            ...eventSlice.actions.fetchEvents(),
-          },
-        ])
       })
     })
   })
