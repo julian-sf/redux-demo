c9b545003dba4f3aebdfa248eb373e3281f6f13f
Julian Coy <julian@coy.cloud>
2020-03-17 14:06:03 -0400

    Cleaned up data locations, added styling, and render counts

---
+++ components/AuthButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { useLoggedIn, useUserName } from '../store/auth'
+import { useRenderCount } from '../utils/useRenderCount'
+
+export const AuthButton = () => {
+  const { loggedIn, login, logout } = useLoggedIn()
+  const userName = useUserName()
+  const renderCount = useRenderCount()
+
+  return (
+    <>
+      <div>
+        <div className={'button-container'}>
+          <button type={'button'} onClick={() => (loggedIn ? logout() : login())}>
+            {loggedIn === 'unknown' ? 'Login' : loggedIn ? 'Logout' : 'Login'}
+          </button>
+        </div>
+        {loggedIn !== 'unknown' && <div className={'user'}>User: {userName ?? '<empty>'}</div>}
+        AuthButton render count: {renderCount}
+      </div>
+      <style jsx>{`
+        div {
+          display: flex;
+        }
+        .button-container {
+          width: 150px;
+        }
+
+        .user {
+          flex: 1;
+        }
+      `}</style>
+    </>
+  )
+}
---
+++ utils/useRenderCount.ts
@@ -0,0 +1,9 @@
+import { useRef } from 'react'
+
+export const useRenderCount = () => {
+  const renderCount = useRef(0)
+
+  renderCount.current += 1
+
+  return renderCount.current
+}
