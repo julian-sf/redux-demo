f9488e3b7099315dc21f537ab00bb8ac7640a873
Julian Coy <julian@coy.cloud>
2020-03-17 11:06:02 -0400

    Fixed quick issue with fetch attempting to run during server render

---
+++ api/index.ts
@@ -0,0 +1,49 @@
+import { EventData } from '../server/data/events'
+
+const HOST = 'http://localhost:3333'
+
+export interface Events {
+  [id: string]: EventData
+}
+
+export const transformEventResponse = (events: EventData[]): Events => {
+  return events.reduce((accumulator, event) => {
+    accumulator[event.id] = event
+
+    return accumulator
+  }, {})
+}
+
+export const fetchEvents = async () => {
+  if (typeof fetch === 'undefined') return {}
+
+  const response = await fetch(`${HOST}/events`, { credentials: 'include' })
+
+  return transformEventResponse(await response.json())
+}
+
+export const fetchLogin = async () => {
+  if (typeof fetch === 'undefined') return {}
+
+  const response = await fetch(`${HOST}/login`, { method: 'POST', credentials: 'include' })
+
+  return await response.json()
+}
+
+export const fetchLogout = async () => {
+  if (typeof fetch === 'undefined') return {}
+
+  return await fetch(`${HOST}/logout`, { method: 'POST', credentials: 'include' })
+}
+
+export const fetchUserStatus = async () => {
+  if (typeof fetch === 'undefined') return {}
+
+  const response = await fetch(`${HOST}/user`, { credentials: 'include' })
+
+  if (!response.ok) {
+    return {}
+  }
+
+  return await response.json()
+}
