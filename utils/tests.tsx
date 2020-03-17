a310dd1f650cc61fb3ae1b1c7186b255536a5009
Julian Coy <julian@coy.cloud>
2020-03-17 16:37:29 -0400

    Update the test to use react-testing-library and test rendering Events

---
+++ utils/tests.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import { initializeStore } from '../store'
+
+export function renderWithRedux(
+  ui,
+  { preloadedState, store = initializeStore(preloadedState) } = { preloadedState: undefined },
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    // adding `store` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    store,
+  }
+}
