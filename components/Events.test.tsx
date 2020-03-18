a41195379a0ea24e7af49ed75eb040035a3ca2aa
Julian Coy <julian@coy.cloud>
2020-03-17 22:15:13 -0400

    Updated tests

--- components/Events.test.tsx
+++ components/Events.test.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 
 import { transformEventResponse } from '../api'
 import { loadEvents } from '../server/data/events'
@@ -8,55 +6,59 @@ import { RootState } from '../store'
 import { renderWithRedux } from '../utils/tests'
 import { Events } from './Events'
 
-const mockStore = configureMockStore<Partial<RootState>>([thunk])
-
-function mockFetchEvents() {
-  const original = jest.requireActual('../api/index')
-
-  return {
-    ...original,
-    fetchEvents: jest.fn(),
-  }
-}
-
-jest.mock('../api/index', () => mockFetchEvents())
+jest.mock('../api/index', () => ({
+  ...jest.requireActual('../api/index'),
+  fetchEvents: jest.fn(),
+}))
 
 const apiMock = jest.requireMock('../api/index')
+const results = transformEventResponse(loadEvents())
 
-describe('events store', () => {
-  const store = mockStore({})
+describe('Events component', () => {
+  // This test can be more cleanly replaced with the following test:
+  //   [store/events.test.tsx] events store > useEvents > fetches when store is not initialized
+  it('fetches when store is not initialized', async () => {
+    // setup mocks
+    apiMock.fetchEvents.mockImplementationOnce(() => results)
 
-  beforeEach(() => {
-    store.clearActions()
+    // dispatch actions
+    const wrapper = renderWithRedux(<Events />)
+
+    // make sure the mock was called
+    expect(apiMock.fetchEvents).toHaveBeenCalled()
+
+    // there should be 5 items on display with names...
+    expect(await wrapper.findAllByText(/Name:/)).toHaveLength(5)
+
+    // verify two of the links generated
+    expect(await wrapper.findByText('Name: David Copperfield')).toHaveProperty(
+      'href',
+      'http://localhost/david-copperfield',
+    )
+
+    expect(await wrapper.findByText('Name: Entertainment')).toHaveProperty(
+      'href',
+      'http://localhost/mgm-resorts-entertainment',
+    )
   })
 
-  // this isn't a very useful test, but it just kinda goes to show how difficult testing redux stores can be with just
-  // dispatches and actions
-  describe('Make sure request completes, and links are rendered correctly', () => {
-    it('getEvents calls the api functions as expected', async () => {
-      // setup mocks
-      const results = transformEventResponse(loadEvents())
-      apiMock.fetchEvents.mockImplementationOnce(() => results)
-
-      // dispatch actions
-      const wrapper = renderWithRedux(<Events />)
-
-      // make sure the mock was called
-      expect(apiMock.fetchEvents).toHaveBeenCalled()
-
-      // there should be 5 items on display with names...
-      expect(await wrapper.findAllByText(/Name:/)).toHaveLength(5)
-
-      // verify two of the links generated
-      expect(await wrapper.findByText('Name: David Copperfield')).toHaveProperty(
-        'href',
-        'http://localhost/david-copperfield',
-      )
-
-      expect(await wrapper.findByText('Name: Entertainment')).toHaveProperty(
-        'href',
-        'http://localhost/mgm-resorts-entertainment',
-      )
+  it('uses store when it is initialized', async () => {
+    const wrapper = renderWithRedux(<Events />, {
+      preloadedState: { events: { initialized: true, data: results, loading: false } } as RootState,
     })
+
+    // there should be 5 items on display with names...
+    expect(await wrapper.findAllByText(/Name:/)).toHaveLength(5)
+
+    // verify two of the links generated
+    expect(await wrapper.findByText('Name: David Copperfield')).toHaveProperty(
+      'href',
+      'http://localhost/david-copperfield',
+    )
+
+    expect(await wrapper.findByText('Name: Entertainment')).toHaveProperty(
+      'href',
+      'http://localhost/mgm-resorts-entertainment',
+    )
   })
 })
---
+++ store/events.test.tsx
@@ -0,0 +1,50 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import { transformEventResponse } from '../api'
+import { loadEvents } from '../server/data/events'
+import { useEvents } from './events'
+
+import { initializeStore } from './index'
+
+jest.mock('../api/index', () => ({
+  ...jest.requireActual('../api/index'),
+  fetchEvents: jest.fn(),
+}))
+
+const apiMock = jest.requireMock('../api/index')
+const results = transformEventResponse(loadEvents())
+
+describe('events store', () => {
+  describe('useEvents', () => {
+    it('fetches when store is not initialized', async () => {
+      apiMock.fetchEvents.mockImplementationOnce(() => results)
+
+      const { result } = renderHook(() => useEvents(), {
+        wrapper: props => (
+          <Provider
+            {...props}
+            store={initializeStore({
+              auth: { isLoggedIn: 'unknown' },
+              events: {
+                initialized: false,
+                loading: false,
+                data: {},
+              },
+            })}
+          />
+        ),
+      })
+
+      // tell react we EXPECT changes to happen on promise resolution and wait for them to finish
+      await act(() => Promise.resolve())
+
+      expect(apiMock.fetchEvents).toHaveBeenCalled()
+      expect(result.error).toBeUndefined()
+      expect(result.current.initialized).toBeTruthy()
+      expect(result.current.loading).toBeFalsy()
+      expect(result.current.events).toEqual(results)
+    })
+  })
+})
