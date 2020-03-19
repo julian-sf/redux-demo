fdd5a8290c45088fb2e3dd2bcf9dae95415c705d
Mateusz Blyszcz <mat@superformula.com>
2020-03-19 19:30:46 +0100

    Fetch events once

--- api/events/useEventsQuery.ts
+++ api/events/useEventsQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/react-hooks';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useAuthContext } from '../../contexts/AuthContext/useAuthContext';
 import { EVENTS_QUERY } from './eventsQuery/eventsQuery';
@@ -7,10 +7,16 @@ import { EventsResponse } from './eventsQuery/eventsQuery.types';
 
 export const useEventsQuery = () => {
   const { userInfo } = useAuthContext();
+  const isMountedRef = useRef(false);
+
   const { data, refetch, loading } = useQuery<EventsResponse>(EVENTS_QUERY, { fetchPolicy: 'cache-first' });
 
   useEffect(() => {
-    refetch();
+    if (isMountedRef.current) {
+      refetch();
+    } else {
+      isMountedRef.current = true;
+    }
   }, [userInfo.isLoggedIn, refetch]);
 
   return {
