2973047386d18bec47db1e27fcce2c009d7a5be9
Julian Coy <julian@superformula.com>
2020-09-24 22:22:03 -0400

    Simplified app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,21 @@
 // This file needs to stay here because of next.js dir structure.
 // Documentation: https://nextjs.org/docs#custom-app
 
-import NextApp from 'next/app';
+import { AppProps } from 'next/app';
 import React from 'react';
 
 import { EventsProvider } from '../comparison/context/EventsContext';
 import { RouterContextProvider } from '../next-utils/router';
 import { wrapper } from '../store/wrapper';
 
-class App extends NextApp {
-  private _renderPage() {
-    const { Component: Page, pageProps } = this.props;
-
-    return <Page {...pageProps} />;
-  }
-
-  render() {
-    return (
-      <RouterContextProvider>
-        <EventsProvider>{this._renderPage()}</EventsProvider>
-      </RouterContextProvider>
-    );
-  }
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <RouterContextProvider>
+      <EventsProvider>
+        <Component {...pageProps} />
+      </EventsProvider>
+    </RouterContextProvider>
+  );
+};
 
 export default wrapper.withRedux(App);
