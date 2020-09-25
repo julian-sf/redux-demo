29a0fdc1af4d62dcbc303eff57e11e8f5bbe932b
Julian Coy <julian@superformula.com>
2020-09-25 01:39:34 -0400

    Made examples similar

--- comparison/context/EventsContainer.tsx
+++ comparison/context/EventsContainer.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useContextSelector } from 'use-context-selector';
 
-import { Events, EventsProps } from '../../shared/Events';
+import { Events } from '../../shared/Events';
 import { EventsContext } from './EventsContext';
 
-export const EventsContainer = ({ specificEventIds }: { specificEventIds?: EventsProps['specificEventIds'] }) => {
+export const EventsContainer = ({ specificEventIds }: { specificEventIds?: string[] }) => {
   const events = useContextSelector(EventsContext, value => value?.data);
 
   return <Events specificEventIds={specificEventIds} events={events} />;
